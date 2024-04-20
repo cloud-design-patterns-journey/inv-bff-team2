2d9b2e079d9fd759639ec739a0ecab5ad6feeecf
NoeSamaille <noe.samaille@ibm.com>
2024-04-20 22:08:43 +0200

    Adds a mock service implementation
    Signed-off-by: NoeSamaille <noe.samaille@ibm.com>

--- src/models/greeting.model.ts
+++ src/models/greeting.model.ts
@@ -1,4 +1,4 @@
-import {Field, ObjectType} from "@nestjs/graphql";
+import { Field, ObjectType } from "@nestjs/graphql";
 
 export interface GreetingModel {
     greeting: string
--- test/controllers/stock-items.controller.spec.ts
+++ test/controllers/stock-items.controller.spec.ts
@@ -1,12 +1,27 @@
 import * as request from 'supertest';
+import { Container } from 'typescript-ioc';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import Mock = jest.Mock;
+
 import { AppModule } from '../../src/app.module';
+import { StockItemsMockService } from '../../src/services';
+
 
 describe('stock-item.controller', () => {
 
     let app: INestApplication;
+    let serviceListStockItems: Mock;
+
     beforeEach(async () => {
+
+        serviceListStockItems = jest.fn();
+        Container.bind(StockItemsMockService).factory(
+            () => ({
+                listStockItems: serviceListStockItems
+            }),
+        );
+
         const moduleFixture: TestingModule = await Test.createTestingModule({
           imports: [AppModule],
         }).compile();
@@ -21,12 +36,13 @@ describe('stock-item.controller', () => {
 
     describe('given GET /stock-items', () => {
         describe('when service is successful', () => {
-            test('then return 200 status', async () => {
-                return request(app.getHttpServer()).get('/stock-items').expect(200);
+            const expectedResult = [{ value: 'val' }];
+            beforeEach(() => {
+                serviceListStockItems.mockResolvedValue(expectedResult);
             });
 
-            test('then should return an empty array', async () => {
-                return request(app.getHttpServer()).get('/stock-items').expect([]);
+            test('then return 200 status', async () => {
+                return request(app.getHttpServer()).get('/stock-items').expect(200);
             });
         });
     });
